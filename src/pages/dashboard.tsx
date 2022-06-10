01174a6cdde6e5c0c2dbe0b9ce8e0b56a9879a22
robertotics4 <robertooliveiragp@gmail.com>
2022-06-10 18:33:16 -0300

    fix props apexchart bug

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,8 +3,9 @@ import { ApexOptions } from "apexcharts";
 import dynamic from 'next/dynamic';
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import { Props } from 'react-apexcharts';
 
-const Chart = dynamic(() => import('react-apexcharts').then((module) => module.default), {
+const Chart = dynamic<Props>(() => import('react-apexcharts').then(module => module.default), {
   ssr: false
 }); 
 
@@ -87,7 +88,7 @@ export default function Dashboard() {
 
           <Box p="8" bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-            <Chart options={options} series={series} type="area" height={160} pb="4"  />
+            <Chart type="area" options={options} series={series}  height={160} pb="4"  />
           </Box>
         </SimpleGrid>
       </Flex>
