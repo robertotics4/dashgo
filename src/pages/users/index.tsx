65557babf6b3606babbd13b123a0f81f6871fb3b
robertotics4 <robertooliveiragp@gmail.com>
2022-07-12 18:35:08 -0300

    finish react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,9 +25,24 @@ import { useQuery } from 'react-query';
 export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
-    const data = response.json();
+    const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.id,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
+  },  {
+    staleTime: 1000 * 5
   });
 
   const isWideVersion = useBreakpointValue({
@@ -80,44 +95,21 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Roberto Oliveira</Text>
-                        <Text fontSize="small" color="gray.300">robertooliveiragp@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de abril, 2021</Td>}
-                  </Tr>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="small" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
 
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Roberto Oliveira</Text>
-                        <Text fontSize="small" color="gray.300">robertooliveiragp@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de abril, 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Roberto Oliveira</Text>
-                        <Text fontSize="small" color="gray.300">robertooliveiragp@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de abril, 2021</Td>}
-                  </Tr>
                 </Tbody>
               </Table>
 
